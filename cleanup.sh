83b8b9eb569314931cf07fb7c12157bd6f6a7c3c
Brandon Stevenson <stevensonb@smu.edu>
2023-06-02 13:54:09 -0500

    .

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -rf data/ksw data/models/ksw data/plots/ksw data/tensorboard/ksw 2>/dev/null
+rm -rf data/ksw/*.nc data/models/ksw data/plots/ksw data/tensorboard/ksw 2>/dev/null
 rm logs/* 2>/dev/null
