41b3977a5e6491595a0012ffdee57b5a27b0b345
Brandon Stevenson <stevensonb@smu.edu>
2023-08-22 12:22:09 -0500

    Cleanup

--- trainer.sh
+++ trainer.sh
@@ -3,6 +3,8 @@
 
 JOB1="scripts/trainer.sbatch"
 
+mkdir -p logs/training
+
 # Submit the first job and capture the job ID
 JOB1_ID=$(sbatch $JOB1 | awk '{print $4}')
 # while squeue -j $JOB1_ID | grep -q $JOB1_ID; do
