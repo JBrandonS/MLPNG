a641361146c356437ba5bf2fe8d3502029bafc9b
Brandon Stevenson <stevensonb@smu.edu>
2024-02-17 14:56:07 -0600

    Improved consts in logging due to time warning

--- scripts/utils/tf/timedloggingcallback.py
+++ scripts/utils/tf/timedloggingcallback.py
@@ -3,75 +3,102 @@ import tensorflow as tf
 
 from tensorflow.keras.callbacks import Callback
 
+
 class TimedLoggingCallback(Callback):
     """
     A custom Keras callback for logging training progress and time.
 
     This callback logs the progress of training along with the time taken for each batch and epoch.
     The frequency of logging can be controlled with the `print_frequency` parameter.
-    The number of batches to log can be limited with the `stop_batch_after` parameter to help with log spam.
 
     Attributes:
         print_frequency (int): The frequency of logging in seconds. Default is 60 seconds.
         last_print_time (float): The last time the log was printed.
         batch_start_time (float): The start time of the current batch.
         epoch_start_time (float): The start time of the current epoch.
-        first_epoch (bool): A flag to check if it's the first epoch.
     """
-        
+
     def __init__(self, print_frequency=60):
         super().__init__()
+
         self.print_frequency = print_frequency
-        self.last_print_time = time.time()
+        self.last_print_time = 0
         self.batch_start_time = 0
         self.epoch_start_time = 0
-        self.first_epoch = True
 
-    def _get_time_str(self, seconds):
-        minutes, seconds = divmod(int(seconds), 60)
-        if minutes >= 60:
-            hours, minutes = divmod(minutes, 60)
-            return f"{hours}:{minutes:02}:{seconds:02}"
-        return f"{minutes}:{seconds:02}" if minutes else f"{seconds}s"
+    def set_params(self, params):
+        super().set_params(params)
+
+        # some small precalcs because tf was yelling about time
+        self._epoch_str_len = len(str(self.params["epochs"]))
+        self._steps_str_len = len(str(self.params["steps"]))
+
+    @staticmethod
+    def _get_time_str(seconds: int):
+        """
+        Convert seconds to a human readable string.
+
+        Examples:
+            _get_time_str(5546) -> "1:32:26"
+            _get_time_str(1946) -> "32:26"
+            _get_time_str(26)   -> "26s"
+        """
+        if seconds >= 60:
+            minutes, seconds = divmod(seconds, 60)
+            if minutes >= 60:
+                hours, minutes = divmod(minutes, 60)
+                return f"{hours}:{minutes:02}:{seconds:02}"
+            return f"{minutes}:{seconds:02}"
+        return f"{seconds:02}s"
+
+    @staticmethod
+    def _get_log_line(logs=None):
+        """
+        Convert the logs to a human readable string of format ' - key: value'.
+        """
+        if logs is None:
+            return ""
+        return " - ".join(f"{k}: {v:.4f}" for k, v in logs.items())
+
+    def on_train_begin(self, logs=None):
+        print("Starting training...")
+        self.last_print_time = time.time()
+
+    def on_train_end(self, logs=None):
+        print("Training complete.")
 
     def on_train_batch_begin(self, batch, logs=None):
         self.batch_start_time = time.time()
 
-    def on_train_batch_end(self, batch, logs=None):        
+    def on_train_batch_end(self, batch, logs=None):
         current_time = time.time()
         if current_time - self.last_print_time >= self.print_frequency:
-            eta = (self.params["steps"] - batch) * (
-                current_time - self.batch_start_time
-            )
+            steps = self.params["steps"]
+
+            eta = (steps - batch) * int(current_time - self.batch_start_time)
             eta = self._get_time_str(eta)
 
-            progress = batch / self.params["steps"]
-            progress_bar = (
-                "=" * int(progress * 30) + ">" + "." * (29 - int(progress * 30))
-            )
+            progress = batch / steps
+            progress_bar_val = int(progress * 30)
+            progress_bar = "=" * progress_bar_val + ">" + "." * (29 - progress_bar_val)
 
-            metrics_log = " - ".join(f"{k}: {v:.4f}" for k, v in logs.items())
+            metrics_log = self._get_log_line(logs)
 
-            batch_str = str(batch).rjust(len(str(self.params["steps"])))
-            print(
-                f"{batch_str}/{self.params['steps']} [{progress_bar}] - ETA: {eta} - {metrics_log}",
-                flush=True,
-            )
+            batch_str = f"{str(batch).rjust(self._steps_str_len)} / {steps}"
+            print(f"{batch_str} [{progress_bar}] - ETA: {eta} - {metrics_log}")
             self.last_print_time = current_time
 
     def on_epoch_begin(self, epoch, logs=None):
         self.epoch_start_time = time.time()
 
-        if self.first_epoch:
-            print("Starting training...", flush=True)
-            self.first_epoch = False
-        
-    def on_epoch_end(self, epoch, logs=None):        
-        elapsed_time = time.time() - self.epoch_start_time
+    def on_epoch_end(self, epoch, logs=None):
+        current_time = time.time()
+        elapsed_time = int(current_time - self.epoch_start_time)
         elapsed_time = self._get_time_str(elapsed_time)
 
-        metrics_log = " - ".join(f"{k}: {v:.4f}" for k, v in logs.items())
+        metrics_log = self._get_log_line(logs)
 
-        epoch_str = str(epoch).rjust(len(str(self.params["epochs"])))
-        print(f"Epoch: {epoch_str} - Time: {elapsed_time} - {metrics_log}", flush=True)
-        self.last_print_time = time.time()
+        epoch_str = str(epoch).rjust(self._epoch_str_len)
+
+        print(f"Epoch: {epoch_str} - Time: {elapsed_time} - {metrics_log}")
+        self.last_print_time = current_time
