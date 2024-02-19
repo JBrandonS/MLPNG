e76b693dc6f268a367c0913c4b6bda5cf6cbd59a
Bradnon Stevenson <stevensonb@smu.edu>
2024-02-19 17:49:39 -0600

    sim config updates

--- datagenerator.sh
+++ datagenerator.sh
@@ -16,35 +16,33 @@ SETTINGS_DIR="settings/"
 # )
 
 SETTINGS=(
-  "ul_nn_128_large.json" #"ul_nn_512_large.json" "ul_nn_1024_large.json" "l_128_large.json"
+  "ul_nn_128_large.json" "l_128_large.json" "ul_nn_512_large.json" "l_512_large.json"
 )
 
 JOB1="sbatch/datagen-large.sbatch"
 JOB2="sbatch/combiner.sbatch"
 JOB3="sbatch/estimator.sbatch"
 
-mkdir -p logs/datagen/ logs/combiner/ logs/estimator/
+# Uncomment to run the heidelberg estimator test
+sbatch "sbatch/heidelberg_est.sbatch"
 
 for x in "${SETTINGS[@]}"
 do
     SETTINGSFILE="$SETTINGS_DIR$x"
 
     # Submit the first job and capture the job ID
-    # JOB1_ID=$(sbatch $JOB1 "$SETTINGSFILE" | awk '{print $4}')
-    # mkdir -p logs/datagen/"$JOB1_ID"
-    # while squeue -j "$JOB1_ID" | grep -q "$JOB1_ID"; do
-    #   sleep 1
-    # done
+    JOB1_ID=$(sbatch $JOB1 "$SETTINGSFILE" | awk '{print $4}')
+    mkdir -p logs/datagen/"$JOB1_ID"
+    while squeue -j "$JOB1_ID" | grep -q "$JOB1_ID"; do
+      sleep 1
+    done
 
-    # JOB2_ID=$(sbatch $JOB2 "$SETTINGSFILE" | awk '{print $4}')
-    # while squeue -j "$JOB2_ID" | grep -q "$JOB2_ID"; do
-    #   sleep 1
-    # done
+    JOB2_ID=$(sbatch $JOB2 "$SETTINGSFILE" | awk '{print $4}')
+    while squeue -j "$JOB2_ID" | grep -q "$JOB2_ID"; do
+      sleep 1
+    done
 
     JOB3_ID=$(sbatch $JOB3 "$SETTINGSFILE" | awk '{print $4}')
 done
 
-while squeue -j "$JOB3_ID" | grep -q "$JOB3_ID"; do
-  sleep 1
-done
-echo "All jobs finsihed."
+echo "All jobs submitted."
