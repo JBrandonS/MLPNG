1d91a8b9b191a81a5bdcf68880b803ca6b9ac913
Bradnon Stevenson <stevensonb@smu.edu>
2024-02-24 17:09:42 -0600

    Removed a lot of unnessary files

--- generator.sh
+++ generator.sh
@@ -7,8 +7,10 @@
 # run with `nohup bash generator.sh &` so you can close the terminal and log out of ssh without killing the process
 #
 
+# direcotry holding all the settings files
 SETTINGS_DIR="settings/"
 
+# list of the settings file to be used, will be ran in order
 SETTINGS=(
   "l_128.json" 
   "l_256.json"
@@ -20,16 +22,24 @@ SETTINGS=(
 # override the json / default settings with command line arguments
 # see config.py for the meaning of these settings
 ARGS=(
-  "--nsims" "10000"
+  "--nsims" "1000"
     # "--lensing"
     # "--disable_lensing"
     # "--noise"
     # "--disable_noise"
     # "--force_alm_gen"
     # "--fnl_range" "-100" "100"
+    # "--narray" "10" # change alm and patch runners from `sbatch $JOB0` to `sbatch --array=1-100 $JOB0`
 )
 
-echo "Submitting jobs with overrides:" "${ARGS[@]}"
+# override the slurm settings with command line arguments
+# only used for alm / patch generation
+SLURM_ARGS=(
+  # "--array" "1-10"
+)
+
+
+echo "Submitting jobs with overrides:" "${ARGS[@]}" "\n" "Slurm overrides:" "${SLURM_ARGS[@]}"
 
 # the sbatch files to be used
 JOB0="sbatch/almgen.sbatch"
@@ -40,23 +50,24 @@ JOB3="sbatch/estimator.sbatch"
 # Uncomment to run the heidelberg estimator test
 # JOBH_ID=$(sbatch "sbatch/heidelberg.sbatch" | awk '{print $4}')
 # echo "Submitted Heidelberg Estimator with ID $JOBH_ID"
-# exit 0
+# # exit 0
 
 # each block submits a slurm job based on the jobs files 
 # and settings files, possibly setups a logging dir if needed
 # then waits for the job to finish before moving on to the next one
+# the awk command is used to extract the job id from the sbatch output
 for x in "${SETTINGS[@]}"
 do
     SETTINGSFILE="$SETTINGS_DIR$x"
 
-    JOB0_ID=$(sbatch $JOB0 "${ARGS[@]}" "$SETTINGSFILE" | awk '{print $4}')
+    JOB0_ID=$(sbatch "${SLURM_ARGS[@]}" $JOB0 "${ARGS[@]}" "$SETTINGSFILE" | awk '{print $4}')
     mkdir -p logs/almgen/"$JOB1_ID"
     echo "Submitted Almgen of $x with ID $JOB0_ID"
     while squeue -j "$JOB0_ID" | grep -q "$JOB0_ID"; do
       sleep 1
     done
 
-    # JOB1_ID=$(sbatch $JOB1 "${ARGS[@]}" "$SETTINGSFILE" | awk '{print $4}')
+    # JOB1_ID=$(sbatch "${SLURM_ARGS[@]}" $JOB1 "${ARGS[@]}" "$SETTINGSFILE" | awk '{print $4}')
     # mkdir -p logs/patchgen/"$JOB1_ID"
     # echo "Submitted patchgen of $x with ID $JOB1_ID"
     # while squeue -j "$JOB1_ID" | grep -q "$JOB1_ID"; do
--- tfds_convert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# used to convert completed data to tdfs
-# trainer is used as a general runner
-# alm or hdf5 can be used as the data source
-# alm-split can be used to generae alm data that is split into real, imag along last dimension
-sbatch "sbatch/trainer.sbatch" "scripts/data_to_tfds.py" "alm-split"
