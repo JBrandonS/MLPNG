c5e8e0dc93497fb819debf29a5cf450f162f20e0
Bradnon Stevenson <stevensonb@smu.edu>
2024-07-09 13:40:40 -0500

    Added CLI to the cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,31 +1,101 @@
 #!/usr/bin/env bash
 
-# a fairly destructive cleaner which will remove the logs and some data files. It does not remove any sim data.
+remove_data=false
+remove_plots=false
+remove_tf=false
+remove_models=false
 
-# remove the jupyter server file if it is not running exists
-if [[ ! $(squeue --me -h -n jupyter) && -f .vscj.out ]]; then
-    rm .vscj.out
+print_help () {
+    echo "Usage: $0 [options]"
+    echo ""
+    echo "A possibly destructive cleaner which will remove the logs and optionally data files."
+    echo ""
+    echo "Options:"
+    echo "  -m, --models    Remove the model data"
+    echo "  -d, --data      Remove the data files"
+    echo "  -p, --plots     Remove all plots"
+    echo "  -t, --tf        Remove all misc tensorflow logs, such as tuning, tensorboard, and wandb logs"
+    echo "  --all           Remove everything"
+    echo "  -h, --help      Print this help message"
+    echo ""
+}
+
+
+# Parse the CLI arguments
+if ! valid_args=$(getopt \
+    -o mdpth \
+    --long models,data,plots,tf,all,help \
+    -- "$@"
+); then
+    # invalid arguments found, print usage and exit
+    print_help
+    exit 1
+fi
+
+# sets up the positional arguments from valid_args for use in the while loop
+eval set -- "$valid_args"
+
+# Here we actually process the CLI arguments
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h | --help)
+        print_help
+        exit 0
+        ;;
+    -m | --models)
+        remove_models=true
+        shift
+        ;;
+    -d | --data)
+        remove_data=true
+        shift
+        ;;
+    -p | --plots)
+        remove_plots=true
+        shift
+        ;;
+    -t | --tf)
+        remove_tf=true
+        shift
+        ;;
+    --all)
+        remove_data=true
+        remove_models=true
+        remove_plots=true
+        remove_tf=true
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+  esac
+done
+
+# remove the jupyter server file if it is not running and exists
+if [[ ! $(squeue --me -h -n jupyter) && -f .jupyter.out ]]; then
+    rm .jupyter.out
 fi
 
 # logs
-rm -rvf logs/combiner/*.log
-rm -rvf logs/estimator/*.log
-rm -rvf logs/heidelberg/*.log
-rm -rvf logs/training/*.log
-rm -rvf logs/tuning/*.log
-rm -rvf logs/generator/*.log
-rm -rvf logs/generator/*/
+rm -rvf logs/*
+
+if [[ "$remove_plots" == true ]]; then
+    rm -rvf data/plots/*
+fi
 
 # some data
-rm -rvf data/models
-rm -rvf data/tensorboard
-rm -rvf data/wandb
-rm -rvf data/tuning
+if [[ "$remove_data" == true ]]; then
+    rm -rvf data/data/*
+    rm -rvf data/kswmc/*
+fi
 
-# jupyter logs
-rm -rvf .jupyter*.out
+if [[ "$remove_models" == true ]]; then
+    rm -rvf data/models/*
+fi
 
-# important data
-# rm -rvf data/data
-# rm -rvf data/kswmc
-# rm -rvf data/plots
+if [[ "$remove_tf" == true ]]; then
+    rm -rvf data/tensorboard
+    rm -rvf data/wandb
+    rm -rvf data/tuning
+fi
