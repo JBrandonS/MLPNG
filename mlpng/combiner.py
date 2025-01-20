ab4f1ea793ced7ee82247ffab38025f70aada65e
Bradnon Stevenson <stevensonb@smu.edu>
2025-01-20 12:01:29 -0600

    Modify logging for combiner

--- mlpng/combiner.py
+++ mlpng/combiner.py
@@ -9,7 +9,7 @@ from tqdm.auto import tqdm
 from . import Core
 from .utils import setup_logging
 
-logger = setup_logging("mlpng.combiner", logging.DEBUG)
+logger = setup_logging("mlpng.combiner", logging.INFO)
 
 
 def extract_number(filename):
