5eb3b3461320fd0d069dc1032c83aae37ece32ab
Bradnon Stevenson <stevensonb@smu.edu>
2025-08-14 16:42:00 -0500

    syncing with mp

--- mlpng/bispectrum/__init__.py
+++ mlpng/bispectrum/__init__.py
@@ -7,11 +7,25 @@ try:
     from .bispectrum import (
         compute_bispectrum,
         compute_trispectrum,
+        valid_triangle,
+        tri,
+        SIntegrand,
+        N2Integrand,
+        DeltaN2Integrand,
+        SN2Integrand,
+        SN2NTIntegrand,
     )
 
     __all__ = [
         "compute_bispectrum",
         "compute_trispectrum",
+        "valid_triangle",
+        "tri",
+        "SIntegrand",
+        "N2Integrand",
+        "DeltaN2Integrand",
+        "SN2Integrand",
+        "SN2NTIntegrand",
     ]
 except ImportError:
     # Module might not be built yet
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
                 "mlpng.bispectrum.bispectrum",
                 sources=["mlpng/bispectrum/bispectrum.pyx"],
                 include_dirs=[numpy.get_include(), "mlpng/bispectrum"],
-                # extra_compile_args=["-Ofast", "-ffast-math", "-march=native"],
+                extra_compile_args=["-Ofast", "-ffast-math", "-march=native"],
                 language="cpp",
             )
         ]
